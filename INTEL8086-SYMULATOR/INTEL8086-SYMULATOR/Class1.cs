39e223e6e9646c5b3ce0ae59f9507f2c47d338d8
Mateusz <papierekm@gmail.com>
2021-04-06 20:01:19 +0200

    event handler, stworzenie odzielnie klasy

---
+++ INTEL8086-SYMULATOR/INTEL8086-SYMULATOR/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace INTEL8086_SYMULATOR
+{
+    class Class1 : INotifyPropertyChanged
+    {
+        private string ax;
+        private string bx;
+        private string cx;
+        private string dx;
+        public string AX
+        {
+            get
+            {
+                return ax;
+            }
+            set
+            {
+                int rejestr;
+                bool res = int.TryParse(value, out rejestr);
+                if (res) ax = value;
+                OnPropertyChanged("AX");
+            }
+        }
+        public string BX
+        {
+            get
+            {
+                return bx;
+            }
+            set
+            {
+                int rejestr;
+                bool res = int.TryParse(value, out rejestr);
+                if (res) bx = value;
+                OnPropertyChanged("BX");
+            }
+        }
+        public string CX
+        {
+            get
+            {
+                return cx;
+            }
+            set
+            {
+                int rejestr;
+                bool res = int.TryParse(value, out rejestr);
+                if (res) cx = value;
+                OnPropertyChanged("CX");
+            }
+        }
+        public string DX
+        {
+            get
+            {
+                return dx;
+            }
+            set
+            {
+                int rejestr;
+                bool res = int.TryParse(value, out rejestr);
+                if (res) dx = value;
+                OnPropertyChanged("DX");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
